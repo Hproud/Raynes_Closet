be0e91613441e3d2a789fdb43b966947438e14b9
Hproud <havin.becca.93@gmail.com>
2024-04-02 23:29:16 -0500

    completed XSRF Cookie route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,9 +2,21 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
+
+//! this is the test route
+// router.get('/hello/world', function(req, res) {
+//   res.cookie('XSRF-TOKEN', req.csrfToken());
+//   res.send('Hello World!');
+// });
+
+
+//! this is adding the XSRF Token Cookie
+router.get("/api/csrf/restore", (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    'XSRF-Token': csrfToken
+  });
 });
 
 module.exports = router;
