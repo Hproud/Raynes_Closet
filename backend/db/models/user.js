3821bdf7421571d9247ea2ce4a27764e99cc713e
Hproud <havin.becca.93@gmail.com>
2024-04-04 17:16:50 -0500

    Add User model scope for user security

---
+++ backend/db/models/user.js
@@ -0,0 +1,74 @@
+"use strict";
+const { Model,Validator } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init(
+    {
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [3, 256],
+          isEmail: true,
+        },
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          len: [4, 30],
+          isNotEmail(value) {
+            if (Validator.isEmail(value)) {
+              throw new Error("Cannot be an email.");
+            }
+          }
+        }
+      },
+      hashedPassword: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [60, 60],
+        },
+      },
+      address: {
+        type: DataTypes.STRING,
+      },
+      city: {
+        type: DataTypes.STRING,
+      },
+      state: {
+        type: DataTypes.STRING,
+      },
+      zipcode: {
+        type: DataTypes.STRING,
+        validate: {
+          len: [5,5]
+        }
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
+  return User;
+};
---
+++ backend/db/seeders/20240404220537-demo-user.js
@@ -0,0 +1,103 @@
+'use strict';
+const {User} = require('../models');
+const bcrypt = require('bcryptjs');
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+
+    await User.bulkCreate([
+      {
+        email: 'demo@user.io',
+        username: 'Demo-lition',
+        hashedPassword: bcrypt.hashSync('password'),
+        firstName: 'Jane',
+        lastName: 'Doe',
+        address: '123 Fake St',
+        city: "new City",
+        state: "Texas",
+        zipcode: '98765'
+      },
+      {
+        email: 'user1@user.io',
+        username: 'FakeUser1',
+        hashedPassword: bcrypt.hashSync('password2'),
+        firstName: 'jake',
+        lastName: 'smith',
+        address: '234 another st',
+        city: 'waynesville',
+        state: 'Nebraska',
+        zipcode: '45678'
+      },
+      {
+        email: 'user2@user.io',
+        username: 'FakeUser2',
+        hashedPassword: bcrypt.hashSync('password3'),
+        firstName: 'jimmy',
+        lastName: 'cool',
+        address: '458 new St',
+        city: 'San Antonio',
+        state: 'Texas',
+        zipcode:'77745'
+      },
+      {
+        email: 'user3@user.io',
+        username: 'FakeUser3',
+        hashedPassword: bcrypt.hashSync('password4'),
+        firstName: 'jason',
+        lastName: 'smith',
+        address: '658 awesome rd',
+        city: 'Kent',
+        state: 'Virginia',
+        zipcode: '60258'
+      },
+      {
+        email: 'user4@user.io',
+        username: 'FakeUser4',
+        hashedPassword: bcrypt.hashSync('password5'),
+        firstName: 'jamie',
+        lastName: 'lester',
+        address: '99 pointed st',
+        city: 'Rogerville',
+        state: 'Ohio',
+        zipcode: '15975'
+      },
+      {
+        email: 'user5@user.io',
+        username: 'FakeUser5',
+        hashedPassword: bcrypt.hashSync('password6'),
+        firstName: 'Stephanie',
+        lastName: 'James',
+        address: '2254 drive 500',
+        city: 'Jasper',
+        state: 'Missouri',
+        zipcode: '65488'
+      },
+      {
+        email: 'user6@user.io',
+        username: 'FakeUser6',
+        hashedPassword: bcrypt.hashSync('password7'),
+        firstName: 'Jesse',
+        lastName: 'James',
+        address: '37652 pleasnt pkwy',
+        city: 'Pleasantville',
+        state: 'California',
+        zipcode:'69691'
+      }
+    ],{validate: true });
+
+  },
+
+  async down (queryInterface, Sequelize) {
+options.tableName = 'Users';
+const Op = Sequelize.Op;
+return queryInterface.bulkDelete(options, {
+  username: { [Op.in]: ['Demo-lition','FakeUser1','FakeUser2']}
+},{});
+  }
+};
