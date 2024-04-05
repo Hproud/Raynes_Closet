0b0f8461ba9bca5d143deec2046ee8100e728294
Hproud <havin.becca.93@gmail.com>
2024-04-04 20:12:50 -0500

    Add User input validation on user signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,16 +1,44 @@
 const express = require('express')
 const bcrypt = require('bcryptjs');
-
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
 const router = express.Router();
 
-
+const validateSignup = [
+  check('email')
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage('Please provide a valid email.'),
+  check('username')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage('Please provide a username with at least 4 characters.'),
+  check('username')
+    .not()
+    .isEmail()
+    .withMessage('Username cannot be an email.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage('Password must be 6 characters or more.'),
+    check('address')
+    .exists({checkFalsy: true}),
+    check('city')
+    .exists({checkFalsy: true}),
+    check('state')
+    .exists({checkFalsy: true}),
+    check('zipcode')
+    .exists({checkFalsy: true})
+    .isLength({min: 5,max: 5}),
+  handleValidationErrors
+];
 
 // Sign up
 router.post(
-    '/',
+    '/',validateSignup,
     async (req, res) => {
       const { email, password, username, firstName, lastName, address, city, state, zipcode } = req.body;
       const hashedPassword = bcrypt.hashSync(password);
