1fca30006ff37dba2487e943e08484bf3e602e12
Hproud <havin.becca.93@gmail.com>
2024-04-06 12:44:34 -0500

    added functionality for profile dropdown

--- frontend/src/components/ProfileButton/ProfileButton.jsx
+++ frontend/src/components/ProfileButton/ProfileButton.jsx
@@ -3,22 +3,42 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { FaUserCircle } from 'react-icons/fa';
 import * as sessionActions from '../../store/session';
+import { useEffect } from 'react';
 
-function ProfileButton({ user }) {
+export default function ProfileButton({ user }) {
   const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
 
   const logout = (e) => {
     e.preventDefault();
     dispatch(sessionActions.logout());
   };
 
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = () => {
+      setShowMenu(false);
+    };
+
+    document.addEventListener('click', closeMenu);
+
+    return () => document.removeEventListener('click', closeMenu);
+  }, [showMenu]);
+
+  const toggleMenu = (e) => {
+    e.stopPropagation(); // Keep click from bubbling up to document and triggering closeMenu
+    setShowMenu(!showMenu);
+  };
+
 
   return (
     <>
     <button>
-      <FaUserCircle />
+            <FaUserCircle onClick={toggleMenu} />
     </button>
-    <ul className="profile-dropdown">
+    {showMenu && (
+    <ul>
       <li>{user.username}</li>
       <li>{user.firstName} {user.lastName}</li>
       <li>{user.email}</li>
@@ -26,8 +46,6 @@ function ProfileButton({ user }) {
         <button onClick={logout}>Log Out</button>
       </li>
     </ul>
+    )}
   </>
-  );
-}
-
-export default ProfileButton;
+  )} //end of return
