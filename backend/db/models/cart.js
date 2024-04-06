497802f59aefdc4eee71624f274a0481f02917be
Hproud <havin.becca.93@gmail.com>
2024-04-06 16:20:21 -0500

    added rel for cart to cartitem

--- backend/db/models/cart.js
+++ backend/db/models/cart.js
@@ -8,7 +8,15 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+    Cart.hasMany(
+      models.CartItem,{
+        foreignKey: 'cart_id',
+        constraints:false,
+        onDelete: 'CASCADE',
+        hooks:true,
+
+      }
+    )
     }
   }
   Cart.init({
