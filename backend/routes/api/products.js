5d5966f9c4f0c041bfb8c7c481b3172d6fcf6fc7
Hproud <havin.becca.93@gmail.com>
2024-04-06 22:11:56 -0500

    starting routes

---
+++ backend/routes/api/products.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const {Product} =require('../../db/models')
+const {requireAuth} = require('../../utils/auth')
+
+
+const router = express.Router()
+
+//TODO--------------------------------------GET ALL PRODUCTS--------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router
